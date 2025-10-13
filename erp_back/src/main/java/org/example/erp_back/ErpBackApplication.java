71d538605149e152c9e92c2527ed2ba6edbfadcc
Nathan Billaud <natbil@orange.fr>
2025-10-13 15:56:20 +0200

    initial commit

---
+++ erp_back/src/main/java/org/example/erp_back/ErpBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.erp_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpBackApplication.class, args);
+    }
+
+}
---
+++ erp_back/src/test/java/org/example/erp_back/ErpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.erp_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
