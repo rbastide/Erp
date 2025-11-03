3949825c3754f5e3322985d9282afd7270797aa7
Louis Biard <louisbiard24@gmail.com>
2025-11-03 17:22:31 +0100

    Amélioration de RessourcePage.vue

--- erp_front/src/components/RessourcePage.vue
+++ erp_front/src/components/RessourcePage.vue
@@ -10,7 +10,7 @@
     <!-- Header de la page contenant la bande rouge !-->
     <header class="page-header">
       <div class="container-nom">
-        <img src="../assets/uploads/logo-unilim.png"><p>Choix ressources</p>
+        <img src="../assets/uploads/Logo_unilim.png"><p>Choix ressources</p>
       </div>
       <div class="contact">Contact</div>
       <div class="date">00/00/00 <br> 12:00:00</div>
@@ -28,7 +28,22 @@
 
       <div class = "container-button">
         <div class ="push-button">
-            <p class = "text-button">RX.XX</p>
+          <p class = "text-button">RX.XX</p>
+        </div>
+        <div class ="push-button">
+          <p class = "text-button">RX.XX</p>
+        </div>
+        <div class ="push-button">
+          <p class = "text-button">RX.XX</p>
+        </div>
+        <div class ="push-button">
+          <p class = "text-button">RX.XX</p>
+        </div>
+        <div class ="push-button">
+          <p class = "text-button">RX.XX</p>
+        </div>
+        <div class ="push-button">
+          <p class = "text-button">RX.XX</p>
         </div>
       </div>
     </main>
@@ -37,144 +52,146 @@
 </template>
 
 <style scoped>
-  header {
-    .page-header {
-      position: absolute;
-      width: 1280px;
-      height: 172px;
-      left: 0px;
-      top: 0px;
-      background: #B51621;
-    }
+/* header */
+.page-header {
+  position: absolute;
+  width: 100%;
+  height: 172px;
+  left: 0px;
+  top: 0px;
+  background: #B51621;
+}
 
-    .container-nom img {
-      position: absolute;
-      width: 127px;
-      height: 127px;
-      left: 64px;
-      top: 22.5px;
-    }
+.container-nom img {
+  position: absolute;
+  width: 127px;
+  height: 127px;
+  left: 64px;
+  top: 22.5px;
+}
 
-    .container-nom p {
-      /* Bonjour,
-    NOM Prénom */
+.container-nom p {
 
-      position: absolute;
-      width: 723px;
-      height: 124px;
-      left: 209px;
-      top: 24px;
+  position: absolute;
+  width: 723px;
+  height: 124px;
+  left: 209px;
+  top: 24px;
 
-      font-family: 'Roboto', sans-serif;
-      font-style: normal;
-      font-weight: 900;
-      font-size: 56px;
-      line-height: 110%;
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 56px;
+  line-height: 110%;
 
-      display: flex;
-      align-items: center;
-      letter-spacing: -0.03em;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.03em;
 
-      color: #FFFFFF;
-    }
+  color: #FFFFFF;
+}
 
-    .quitter {
+.quitter {
 
-      position: absolute;
-      width: 48px;
-      height: 48px;
-      left: 1192px;
-      top: 64px;
-    }
+  position: absolute;
+  width: 48px;
+  height: 48px;
+  right: 5%;
+  top: 64px;
+}
 
-    .contact {
-      position: absolute;
-      width: 126px;
-      height: 52px;
-      left: 774px;
-      top: 60px;
-      font-family: 'Roboto', sans-serif;
-      font-style: normal;
-      font-weight: 500;
-      font-size: 36px;
-      line-height: 145%;
-      display: flex;
-      align-items: center;
-      text-align: center;
-      letter-spacing: -0.005em;
-      text-transform: capitalize;
-      color: #FFFFFF;
-    }
+.contact {
+  position: absolute;
+  width: 126px;
+  height: 52px;
+  right: 25%;
+  top: 60px;
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 36px;
+  line-height: 145%;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: -0.005em;
+  text-transform: capitalize;
+  color: #FFFFFF;
+}
 
-    .date {
-      position: absolute;
-      width: 100px;
-      height: 70px;
-      left: 940px;
-      top: 51px;
-      font-family: 'Roboto', sans-serif;
-      font-style: normal;
-      font-weight: 500;
-      font-size: 24px;
-      line-height: 145%;
-      display: flex;
-      align-items: center;
-      text-align: center;
-      letter-spacing: -0.005em;
-      text-transform: capitalize;
-      color: #FFFFFF;
-    }
-  }
-  main{
-    .choisir-ressource {
-      position: absolute;
-      width: 672px;
-      height: 66px;
-      left: 38px;
-      top: 196px;
+.date {
+  position: absolute;
+  width: 100px;
+  height: 70px;
+  right: 18%;
+  top: 51px;
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 145%;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: -0.005em;
+  text-transform: capitalize;
+  color: #FFFFFF;
+}
 
-      font-family: 'Roboto', sans-serif;
-      font-style: normal;
-      font-weight: 400;
-      font-size: 32px;
-      line-height: 38px;
 
-      color: #E92533;
-    }
+/* main */
+.choisir-ressource {
+  position: absolute;
+  width: 672px;
+  height: 66px;
+  left: 38px;
+  top: 196px;
 
-    .push-button {
-      /* Push Button */
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 38px;
 
-      box-sizing: border-box;
+  color: #E92533;
+}
 
-      /* Auto layout */
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 0px 16px;
-      isolation: isolate;
+.container-button {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  padding: 10px;
+}
 
-      position: absolute;
-      width: 360px;
-      height: 200px;
-      left: 38px;
-      top: 295px;
+.push-button {
+  /* Push Button */
 
-      background: #D9D9D9;
-      border: 1px solid rgba(0, 0, 0, 0.25);
-      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-      border-radius: 15px;
-    }
+  box-sizing: border-box;
 
-    .text-button {
-      font-family: 'Roboto', sans-serif;
-      font-weight: 510;
-      font-size: 64px;
-      text-align: center;         /* Texte centré à l'intérieur */
-      color: #B51621;
-      margin: 0;
-    }
-  }
+  /* Auto layout */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 16px;
+  isolation: isolate;
+
+  margin : 10px;
+  font-size: 30px;
+  text-align: center;
+
+  background: #D9D9D9;
+  border: 1px solid rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+}
+
+.text-button {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 510;
+  font-size: 64px;
+  text-align: center;         /* Texte centré à l'intérieur */
+  color: #B51621;
+  margin: 0;
+}
 
 </style>
