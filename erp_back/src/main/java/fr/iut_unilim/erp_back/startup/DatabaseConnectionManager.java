93419691f88d972f5bc6c551cdf65f10ae604c74
hugob <hugo.bequet@laposte.net>
2025-11-17 14:43:08 +0100

    Mise à jour des variables d'environnement de SSL à SSH

--- erp_back/src/main/java/fr/iut_unilim/erp_back/startup/DatabaseConnectionManager.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/startup/DatabaseConnectionManager.java
@@ -15,13 +15,13 @@ public class DatabaseConnectionManager {
     private DatabaseConnectionManager() {}
 
     private static final String[] DATABASE_ENVIRONMENT_VARIABLES = {"DB_HOST", "DB_PORT", "DB_NAME", "DB_USERNAME", "DB_PASSWORD", "SGBD"};
-    private static final String[] SSH_ENVIRONMENT_VARIABLES = {"SSL_USERNAME", "SSL_PASSWORD", "DB_IP", "DB_HOST", "DB_PORT"};
+    private static final String[] SSH_ENVIRONMENT_VARIABLES = {"SSH_USERNAME", "SSH_PASSWORD", "DB_IP", "DB_HOST", "DB_PORT"};
 
     private static void connectSSH(Map<String, String> envVariables) throws JSchException {
         JSch jsch = new JSch();
         int dbPort = Integer.parseInt(envVariables.get("DB_PORT"));
-        Session session = jsch.getSession(envVariables.get("SSL_USERNAME"), envVariables.get("DB_IP"), 22);
-        session.setPassword(envVariables.get("SSL_PASSWORD"));
+        Session session = jsch.getSession(envVariables.get("SSH_USERNAME"), envVariables.get("DB_IP"), 22);
+        session.setPassword(envVariables.get("SSH_PASSWORD"));
         session.setConfig("StrictHostKeyChecking", "no");
         session.setPortForwardingL(dbPort, envVariables.get("DB_HOST"), 3306);
         session.connect();
