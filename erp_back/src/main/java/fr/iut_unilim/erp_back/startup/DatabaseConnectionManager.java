c61a4c6f2ae9b59a31b26a93708ea3a99170819f
hugob <hugo.bequet@laposte.net>
2025-11-17 14:48:20 +0100

    Correction magic numbers

--- erp_back/src/main/java/fr/iut_unilim/erp_back/startup/DatabaseConnectionManager.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/startup/DatabaseConnectionManager.java
@@ -17,13 +17,16 @@ public class DatabaseConnectionManager {
     private static final String[] DATABASE_ENVIRONMENT_VARIABLES = {"DB_HOST", "DB_PORT", "DB_NAME", "DB_USERNAME", "DB_PASSWORD", "SGBD"};
     private static final String[] SSH_ENVIRONMENT_VARIABLES = {"SSH_USERNAME", "SSH_PASSWORD", "DB_IP", "DB_HOST", "DB_PORT"};
 
+    private static final int SSH_PORT = 22;
+    private static final int DB_PORT_RECEIVER = 3306;
+
     private static void connectSSH(Map<String, String> envVariables) throws JSchException {
         JSch jsch = new JSch();
         int dbPort = Integer.parseInt(envVariables.get("DB_PORT"));
-        Session session = jsch.getSession(envVariables.get("SSH_USERNAME"), envVariables.get("DB_IP"), 22);
+        Session session = jsch.getSession(envVariables.get("SSH_USERNAME"), envVariables.get("DB_IP"), SSH_PORT);
         session.setPassword(envVariables.get("SSH_PASSWORD"));
         session.setConfig("StrictHostKeyChecking", "no");
-        session.setPortForwardingL(dbPort, envVariables.get("DB_HOST"), 3306);
+        session.setPortForwardingL(dbPort, envVariables.get("DB_HOST"), DB_PORT_RECEIVER);
         session.connect();
     }
 
