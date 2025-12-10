f37f7f7d03e2f45231b4f048338d44130b2edf58
hugob <hugo.bequet@laposte.net>
2025-12-10 13:53:30 +0100

    Ajout de configuration pour le port back end

--- erp_front/src/services/api.js
+++ erp_front/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const BACK_END_IP = import.meta.env.VITE_BACK_END_IP;
+const BACK_END_PORT = import.meta.env.VITE_PORT_BACK_END;
 
 const apiClient = axios.create({
-    baseURL: `https://${BACK_END_IP}:443/api`,
+    baseURL: `https://${BACK_END_IP}:${BACK_END_PORT}/api`,
     headers: {
         'Content-Type': 'application/json'
     }
