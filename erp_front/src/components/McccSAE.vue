f6f0677345fb994507ea328df0c62ef54afe6dd1
Louis Biard <louisbiard24@gmail.com>
2025-12-15 16:35:44 +0100

    Terminé FillRessourcePage.vue + amélioraition de McccFillHour.vue, McccSAE.vue, McccSummary.vue, ServiceAidePage.vue

--- erp_front/src/components/McccSAE.vue
+++ erp_front/src/components/McccSAE.vue
@@ -86,9 +86,6 @@ function duplicateGreySquare(containerId){
       setupInputLimits();
     });
   }
-  else{
-    console.error("Impossible de trouver le carré gris à dupliquer.");
-  }
 }
 </script>
 
