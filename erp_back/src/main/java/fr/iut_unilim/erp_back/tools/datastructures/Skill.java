a52ba6d0764d19cb420e12142d24aa31b7294a9b
hugob <hugo.bequet@laposte.net>
2025-12-16 16:41:47 +0100

    Changement qu'une ressource puisse avoir plusieurs UE

--- erp_back/src/main/java/fr/iut_unilim/erp_back/tools/datastructures/Skill.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/tools/datastructures/Skill.java
@@ -1,7 +1,7 @@
 package fr.iut_unilim.erp_back.tools.datastructures;
 
 public class Skill {
-    private String ue;
+    private String[] ue;
     private String[] niveaux;
     private String[] acs;
     private String[] acsGrouped;
