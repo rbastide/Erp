19e568a4df3a8fcd917757f942e1fd0d759c6968
hugob <hugo.bequet@laposte.net>
2026-01-05 09:17:10 +0100

    Fix du type de l'id

--- erp_back/src/main/java/fr/iut_unilim/erp_back/repository/McccRepository.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/repository/McccRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface McccRepository extends JpaRepository<Mccc, Long> {
+public interface McccRepository extends JpaRepository<Mccc, McccId> {
     Mccc findByMcccId(McccId mcccId);
 }
--- erp_back/src/main/java/fr/iut_unilim/erp_back/service/McccService.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/service/McccService.java
@@ -1,7 +1,11 @@
 package fr.iut_unilim.erp_back.service;
 
+import fr.iut_unilim.erp_back.entity.Mccc;
 import fr.iut_unilim.erp_back.repository.McccRepository;
+import fr.iut_unilim.erp_back.tools.datastructures.McccId;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class McccService {
@@ -9,5 +13,9 @@ public class McccService {
 
     public McccService(McccRepository mcccRepository) {
         this.mcccRepository = mcccRepository;
+    }
+
+    public Optional<Mccc> getMcccById(McccId id) {
+        return mcccRepository.findById(id);
     }
 }
