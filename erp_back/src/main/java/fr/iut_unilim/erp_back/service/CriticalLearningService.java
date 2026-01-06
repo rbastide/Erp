74c2d6332fccf8e37aa785d7088889b58f2d49c6
hugob <hugo.bequet@laposte.net>
2026-01-06 11:39:04 +0100

    Ajout d'une méthode de save dans le service CriticalLearningService

--- erp_back/src/main/java/fr/iut_unilim/erp_back/service/CriticalLearningService.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/service/CriticalLearningService.java
@@ -1,5 +1,6 @@
 package fr.iut_unilim.erp_back.service;
 
+import fr.iut_unilim.erp_back.entity.CriticalLearning;
 import fr.iut_unilim.erp_back.repository.CriticalLearningRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +12,8 @@ public class CriticalLearningService {
     private final CriticalLearningRepository criticalLearningRepository;
 
     public CriticalLearningService(CriticalLearningRepository criticalLearningRepository) {this.criticalLearningRepository = criticalLearningRepository;}
+
+    public void save(CriticalLearning criticalLearning) {
+        criticalLearningRepository.save(criticalLearning);
+    }
 }
