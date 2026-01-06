a9838261dda064fae580f3bb81d3cec8ee078795
Rémi Bastide <remi.bastide@etu.unilim.fr>
2026-01-06 10:02:35 +0100

    Ajout du service Sae et de la gestion du dto des Sae dans le mcccController

--- erp_back/src/main/java/fr/iut_unilim/erp_back/service/SaeService.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/service/SaeService.java
@@ -1,6 +1,7 @@
 package fr.iut_unilim.erp_back.service;
 
 import fr.iut_unilim.erp_back.entity.Sae;
+import fr.iut_unilim.erp_back.entity.Teacher;
 import fr.iut_unilim.erp_back.repository.SaeRepository;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +19,8 @@ public class SaeService {
     public List<Sae> getSaeByNum(String num) {
         return saeRepository.findByNum(num);
     }
+
+    public void save(Sae sae) {saeRepository.save(sae);}
+
+
 }
