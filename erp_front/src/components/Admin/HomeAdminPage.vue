37fb8c0aa20623fcc3b4b48f1d5bdc837985c71f
Nathan Billaud <natbil@orange.fr>
2026-01-06 10:45:14 +0100

    ajout de la page Gestion des SAEs

--- erp_front/src/components/Admin/HomeAdminPage.vue
+++ erp_front/src/components/Admin/HomeAdminPage.vue
@@ -23,7 +23,11 @@ const handleCompetenceCreating = () => {
 
 const handleResources = () => {
   router.push('/resources-gestion');
-}
+};
+
+const handleAddSae = () => {
+  router.push('/sae-gestion');
+};
 </script>
 
 <template>
@@ -67,6 +71,13 @@ const handleResources = () => {
             <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
           </div>
           <p>Gestion des ressources</p>
+        </div>
+
+        <div class="card-action" @click="handleAddSae">
+          <div class="icon-circle">
+            <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
+          </div>
+          <p>Gestion des SAEs</p>
         </div>
 
       </div>
