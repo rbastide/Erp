da054f849a805c7925022bce3a6a03178dfc08a6
hugob <hugo.bequet@laposte.net>
2026-01-06 15:37:41 +0100

    Fix du back

--- erp_back/src/main/java/fr/iut_unilim/erp_back/service/RankService.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/service/RankService.java
@@ -1,7 +1,10 @@
 package fr.iut_unilim.erp_back.service;
 
+import fr.iut_unilim.erp_back.entity.Rank;
 import fr.iut_unilim.erp_back.repository.RankRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -11,5 +14,13 @@ public class RankService {
 
     public RankService(RankRepository rankRepository) {
         this.rankRepository = rankRepository;
+    }
+
+    public List<Rank> getRanksByNum(int skillNum) {
+        return rankRepository.findByRankNum(skillNum);
+    }
+
+    public boolean doRankNumExists(int skillNum) {
+        return !getRanksByNum(skillNum).isEmpty();
     }
 }
