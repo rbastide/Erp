4ad130aa1bb7a2d8227fdd9f196fc098c2c2883d
Reia TAERO <reiahoani.taero@etu.unilim.fr>
2026-01-08 11:09:11 +0100

    Ajout getAllResourceSheets dans ResourceSheetService

--- erp_back/src/main/java/fr/iut_unilim/erp_back/service/ResourceSheetService.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/service/ResourceSheetService.java
@@ -1,5 +1,6 @@
 package fr.iut_unilim.erp_back.service;
 
+import fr.iut_unilim.erp_back.entity.ResourceSheet;
 import fr.iut_unilim.erp_back.repository.ResourceSheetRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -11,5 +12,9 @@ public class ResourceSheetService {
 
     public ResourceSheetService(ResourceSheetRepository resourceSheetRepository) {
         this.resourceSheetRepository = resourceSheetRepository;
+    }
+
+    public List<ResourceSheet> getAllResourceSheets() {
+        return resourceSheetRepository.findAll();
     }
 }
