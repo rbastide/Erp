c356870acd3b6c98f125559d9c4d7dc8a56b42d8
Rémi Bastide <remi.bastide@etu.unilim.fr>
2026-01-08 11:10:27 +0100

    Ajout du getResourceSheet

--- erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/ResourceSheetController.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/ResourceSheetController.java
@@ -1,6 +1,9 @@
 package fr.iut_unilim.erp_back.controllers;
 
+import fr.iut_unilim.erp_back.entity.ResourceSheet;
 import fr.iut_unilim.erp_back.service.ResourceSheetService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +16,12 @@ public class ResourceSheetController {
     public ResourceSheetController(ResourceSheetService resourceSheetService) {
         this.resourceSheetService = resourceSheetService;
     }
+
+    @GetMapping("/resourceSheet")
+    public ResponseEntity<?> getResourceSheet() {
+        return ResponseEntity.ok(resourceSheetService.getAllResourceSheets());
+    }
+
+
+
 }
