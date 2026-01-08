4836960b4a1d6a96807f0be9eb1aedaabc0885d8
hugob <hugo.bequet@laposte.net>
2026-01-08 10:09:49 +0100

    Ajout de l'envoi du rôle à la connexion

--- erp_back/src/main/java/fr/iut_unilim/erp_back/dto/AuthResponse.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/dto/AuthResponse.java
@@ -2,10 +2,15 @@ package fr.iut_unilim.erp_back.dto;
 
 public class AuthResponse {
     private String token;
+    private String role;
     private String type = "Bearer";
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public void setType(String type) {
