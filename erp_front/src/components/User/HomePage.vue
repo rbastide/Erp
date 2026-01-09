f94bff7c16d6116bdea7dc73996fb4a8ef6b7212
Nathan Billaud <natbil@orange.fr>
2026-01-09 19:09:25 +0100

    correction d'une faute d'orthographe

--- erp_front/src/components/User/HomePage.vue
+++ erp_front/src/components/User/HomePage.vue
@@ -31,7 +31,7 @@ const handleAfficher = () => {
         <div class="icon-circle">
           <svg width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="3" x2="9" y2="21"></line></svg>
         </div>
-        <p>Afficher les fiches ressource</p>
+        <p>Afficher les fiches ressources</p>
       </div>
     </main>
   </div>
