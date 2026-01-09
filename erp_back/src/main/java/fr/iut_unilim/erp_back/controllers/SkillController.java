6866fee6c761f5a599cc685ef72565fdc86c7587
Rémi Bastide <remi.bastide@etu.unilim.fr>
2026-01-09 09:14:47 +0100

    Modification de la route des compétences

--- erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/SkillController.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/SkillController.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/skill")
 public class SkillController {
 
     private final SkillService skillService;
