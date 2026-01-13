f6305027f64ab71df7e0c0163b390b93e2c6b550
Reia TAERO <reiahoani.taero@etu.unilim.fr>
2026-01-13 08:32:37 +0100

    Refacto warning

--- erp_back/src/main/java/fr/iut_unilim/erp_back/service/HourlyVolumeService.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/service/HourlyVolumeService.java
@@ -2,10 +2,8 @@ package fr.iut_unilim.erp_back.service;
 
 import fr.iut_unilim.erp_back.entity.HourlyVolume;
 import fr.iut_unilim.erp_back.repository.HourlyVolumeRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,6 +30,5 @@ public class HourlyVolumeService {
 
     public Optional<HourlyVolume> findById(Long id) {
         return hourlyVolumeRepository.findById(id);
-
     }
 }
