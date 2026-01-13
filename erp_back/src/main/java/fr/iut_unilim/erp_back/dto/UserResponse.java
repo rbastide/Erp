ddbb470993b8c441cab99e88be00af662f0b4cb5
Rémi Bastide <remi.bastide@etu.unilim.fr>
2026-01-13 10:27:33 +0100

    Ajout des email, lastname et firstname dans la réception des données des utilisateurs

--- erp_back/src/main/java/fr/iut_unilim/erp_back/dto/UserResponse.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/dto/UserResponse.java
@@ -1,5 +1,5 @@
 package fr.iut_unilim.erp_back.dto;
 
-public record UserResponse(Long id, String identifier, String role) {
+public record UserResponse(Long id, String identifier, String role, String email, String firstName, String lastName) {
 
 }
