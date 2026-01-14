8969990f92a1cd0bfcec8e77e645cf2ca2bd3ee9
Nathan Billaud <natbil@orange.fr>
2026-01-14 09:21:11 +0100

    rajout des fill="none" dans la partie Information

--- erp_front/src/components/Information/ModifSaved.vue
+++ erp_front/src/components/Information/ModifSaved.vue
@@ -34,7 +34,7 @@ const handleRetour = () => {
         <div class="success-card">
 
           <div class="icon-circle success">
-            <svg width="60" height="60" viewBox="0 0 24 24" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+            <svg width="60" height="60" viewBox="0 0 24 24" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none">
               <polyline points="20 6 9 17 4 12"></polyline>
             </svg>
           </div>
