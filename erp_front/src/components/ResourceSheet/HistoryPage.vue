3f2b65eda6ad66f2babd23b41dc8365d8fe39a40
hugob <hugo.bequet@laposte.net>
2026-01-15 09:34:50 +0100

    WIP liaison PDF front

--- erp_front/src/components/ResourceSheet/HistoryPage.vue
+++ erp_front/src/components/ResourceSheet/HistoryPage.vue
@@ -54,8 +54,9 @@ const handleShow = (item: any) => {
   });
 };
 
-const handleExportPdf = (code: string) => {
-  console.log(`Export PDF demandé pour ${code}`);
+const handleExportPdf = (id: number) => {
+  api.get(`/pdf/resource-sheet/${id}`);
+  console.log(`Export PDF demandé pour ${id}`);
 };
 
 const clearSearch = () => searchQuery.value = '';
@@ -92,7 +93,7 @@ const clearSearch = () => searchQuery.value = '';
               <span class="version-date">{{ formatDate(item.date) }}</span>
             </div>
 
-            <button class="btn-icon-container" @click.stop="handleExportPdf(item.resourceCode)" title="Exporter en PDF">
+            <button class="btn-icon-container" @click.stop="handleExportPdf(item.sheetID)" title="Exporter en PDF">
               <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-file-earmark-arrow-down btn-icon" viewBox="0 0 16 16" fill="none">
                 <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
                 <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
