9a584e827249b5a417012f962a3c2770d03ba2c6
hugob <140191575+flastar-fr@users.noreply.github.com>
2026-01-16 01:40:38 +0100

    ajout des départements sur les ressources

--- erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/ResourceController.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/ResourceController.java
@@ -1,16 +1,18 @@
 package fr.iut_unilim.erp_back.controllers;
 
 import fr.iut_unilim.erp_back.dto.ResourceResponse;
+import fr.iut_unilim.erp_back.entity.Connection;
 import fr.iut_unilim.erp_back.entity.Resource;
 import fr.iut_unilim.erp_back.repository.ResourceRepository;
+import fr.iut_unilim.erp_back.service.ConnectionService;
 import fr.iut_unilim.erp_back.service.ResourceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
 
 @RestController
 @RequestMapping("/api/resources")
@@ -18,10 +20,12 @@ public class ResourceController {
 
     private final ResourceService resourceService;
     private final ResourceRepository resourceRepository;
+    private final ConnectionService connectionService;
 
-    public ResourceController(ResourceService resourceService, ResourceRepository resourceRepository) {
+    public ResourceController(ResourceService resourceService, ResourceRepository resourceRepository, ConnectionService connectionService) {
         this.resourceService = resourceService;
         this.resourceRepository = resourceRepository;
+        this.connectionService = connectionService;
     }
 
     @GetMapping("/resources")
@@ -32,7 +36,7 @@ public class ResourceController {
 
     @PostMapping("/editResource")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public ResponseEntity<?> editResources(@RequestBody List<ResourceResponse> resList) {
+    public ResponseEntity<?> editResources(@RequestBody List<ResourceResponse> resList, Authentication authentication) {
         for (ResourceResponse res : resList) {
             if (res.getResourceID() != null) {
                 Optional<Resource> existingResource = resourceRepository.findById(res.getResourceID());
@@ -50,9 +54,18 @@ public class ResourceController {
             newResource.setNum(res.getNum());
             newResource.setName(res.getName());
             newResource.setSemester(res.getSemestre());
+            handleDepartment(newResource, authentication);
             resourceRepository.save(newResource);
         }
         return ResponseEntity.ok().body("Ressources traitées avec succès");
+    }
+
+    private void handleDepartment(Resource resource, Authentication authentication) {
+        Connection connection = connectionService.findByIdentifier(authentication.getName());
+
+        if (connection == null) return;
+
+        resource.setUniversityDepartment(connection.getUniversityDepartment());
     }
 
 
