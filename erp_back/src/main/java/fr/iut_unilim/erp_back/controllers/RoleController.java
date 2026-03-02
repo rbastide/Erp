39d42e27d931ebe3fc305c6a529e42b163360ef5
hbequet <206597685+hbequet@users.noreply.github.com>
2026-03-02 22:27:36 +0100

    modification des permissions de RoleController vers le nouveau système

--- erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/RoleController.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/RoleController.java
@@ -3,7 +3,11 @@ package fr.iut_unilim.erp_back.controllers;
 import fr.iut_unilim.erp_back.dto.CreateRoleRequest;
 import fr.iut_unilim.erp_back.service.PermissionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/role")
@@ -15,6 +19,7 @@ public class RoleController {
     }
 
     @PostMapping
+    @PreAuthorize("@securityService.hasPermission('PERM_MANAGEMENT')")
     public ResponseEntity<?> createRole(@RequestBody CreateRoleRequest createRoleRequest) {
         return ResponseEntity.ok(permissionService.createPermission(createRoleRequest));
     }
