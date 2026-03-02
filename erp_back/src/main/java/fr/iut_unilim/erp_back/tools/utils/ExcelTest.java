0275c63229e4c50a66e66655504cdd9d743eb5a6
Reia TAERO <reiahoani.taero@etu.unilim.fr>
2026-03-02 17:03:36 +0100

    Conversion d'excel pour les MCCC (première phase - ne pas toucher)

---
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/tools/utils/ExcelTest.java
@@ -0,0 +1,8 @@
+package fr.iut_unilim.erp_back.tools.utils;
+
+public class ExcelTest {
+
+    public static void main(String[] args) {
+        ExcelReader.readExcelFile("C:\\Users\\taero\\Documents\\IUT\\SAE\\ERP_IUT\\INFO_MCCC BUT_2025-2026.xlsx");
+    }
+}
