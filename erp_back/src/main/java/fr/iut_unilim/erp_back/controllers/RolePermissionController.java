1932dcbdfc37269da21f8cf5b612d790ac54e893
hbequet <206597685+hbequet@users.noreply.github.com>
2026-03-09 14:12:06 +0100

    fix d'un message d'erreur du back

--- erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/RolePermissionController.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/RolePermissionController.java
@@ -1,5 +1,6 @@
 package fr.iut_unilim.erp_back.controllers;
 
+import fr.iut_unilim.erp_back.ErpBackApplication;
 import fr.iut_unilim.erp_back.dto.EditRolePermissionRequest;
 import fr.iut_unilim.erp_back.dto.PermissionDefinitionResponse;
 import fr.iut_unilim.erp_back.dto.RolePermissionResponse;
@@ -41,9 +42,10 @@ public class RolePermissionController {
     @PostMapping("/perms/role")
     @PreAuthorize("@securityService.hasPermission('PERM_MANAGEMENT')")
     public ResponseEntity<?> editRolePermissions(@RequestBody List<EditRolePermissionRequest> editRolePermissionRequests) {
+        ErpBackApplication.LOGGER.info("test");
         boolean doErrorOccured = false;
         for (EditRolePermissionRequest editRolePermissionRequest : editRolePermissionRequests) {
-            doErrorOccured |= permissionService.editRolePermission(editRolePermissionRequest);
+            doErrorOccured |= !permissionService.editRolePermission(editRolePermissionRequest);
         }
 
         if (doErrorOccured) {
