6c474958b3b10beaa482aa0d6067444c1d60e77f
Rémi Bastide <remi.bastide@etu.unilim.fr>
2026-03-09 16:36:58 +0100

    Suppression Crossorigins dans ResourceSheetController

--- erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/ResourceSheetController.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/controllers/ResourceSheetController.java
@@ -15,7 +15,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/resourceSheet")
-@CrossOrigin(origins = "http://localhost:5173")
 public class ResourceSheetController {
 
     private final ResourceSheetService resourceSheetService;
