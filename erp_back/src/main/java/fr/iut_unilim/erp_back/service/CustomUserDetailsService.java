9299bd34eedc051ec3b1928dfe1826ec38773a0a
hbequet <206597685+hbequet@users.noreply.github.com>
2026-03-16 16:57:29 +0100

    suppression imports inutiles

--- erp_back/src/main/java/fr/iut_unilim/erp_back/service/CustomUserDetailsService.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/service/CustomUserDetailsService.java
@@ -1,9 +1,7 @@
 package fr.iut_unilim.erp_back.service;
 
-import fr.iut_unilim.erp_back.ErpBackApplication;
 import fr.iut_unilim.erp_back.entity.Connection;
 import fr.iut_unilim.erp_back.repository.ConnectionRepository;
-import org.apache.poi.ddf.EscherRGBProperty;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,7 +9,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
