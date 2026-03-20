0920762aaed5674f41b86e7e2a4e6b888acd40b0
Nathan Billaud <natbil@orange.fr>
2026-03-20 11:28:40 +0100

    menu MCCC (WIP) pour de vrai

--- erp_front/src/components/Mccc/McccMenuPage.vue
+++ erp_front/src/components/Mccc/McccMenuPage.vue
@@ -81,7 +81,7 @@ onMounted(() => {
           <div class="icon-circle">
             <svg width="35" height="35" viewBox="0 0 24 24"  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="fill: none;"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
           </div>
-          <p>Référent(s)</p>
+          <p>Enseignant(s)</p>
         </div>
 
       </div>
