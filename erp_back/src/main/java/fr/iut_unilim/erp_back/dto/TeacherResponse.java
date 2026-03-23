9b26ee999046b038fc2050ff99863dc84c98a1c5
hbequet <206597685+hbequet@users.noreply.github.com>
2026-03-23 11:37:30 +0100

    implémentation d'une route pour récupérer tous les professeurs d'un département

--- erp_back/src/main/java/fr/iut_unilim/erp_back/dto/TeacherResponse.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/dto/TeacherResponse.java
@@ -1,4 +1,9 @@
 package fr.iut_unilim.erp_back.dto;
 
-public record TeacherResponse(String lastname, String firstname) {
+import fr.iut_unilim.erp_back.entity.Connection;
+
+public record TeacherResponse(Long id, String lastname, String firstname) {
+    public TeacherResponse(Connection connection) {
+        this(connection.getId(), connection.getLastName(), connection.getFirstName());
+    }
 }
