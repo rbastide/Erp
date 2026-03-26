1621456f6573faa8f34bd15c10d20438941a9399
Louis Biard <louisbiard24@gmail.com>
2026-03-26 11:14:49 +0100

    Changement de la création des utilisateurs

--- erp_front/src/components/Admin/CreateNewUserPage.vue
+++ erp_front/src/components/Admin/CreateNewUserPage.vue
@@ -1,6 +1,6 @@
 <script setup>
-import {onMounted, ref} from 'vue';
-import {useRouter} from 'vue-router';
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
 import AuthService from '../../services/AuthService.js';
 import AppHeader from '../App/Header.vue';
 import Sidebar from '../App/Sidebar.vue';
@@ -9,15 +9,8 @@ import api from "@/services/api.js";
 const router = useRouter();
 
 const username = ref('');
-const firstname = ref('');
-const universityDepartment = ref('');
-const lastname = ref('');
-const email = ref('');
-const password = ref('');
-const confirmPassword = ref('');
 const role = ref('');
 const errorMessage = ref('');
-const universityDepartments = ref([]);
 const roles = ref([]);
 
 const handleBack = () => {
@@ -27,31 +20,20 @@ const handleBack = () => {
 const handleRegister = async () => {
   errorMessage.value = '';
 
-  if (password.value !== confirmPassword.value) {
-    errorMessage.value = "Les mots de passe ne correspondent pas.";
-    return;
-  }
-
-  if (!username.value || !password.value || !role.value || !firstname.value || !lastname.value || !email.value || !universityDepartment.value) {
+  if (!username.value || !role.value) {
     errorMessage.value = "Veuillez remplir tous les champs.";
     return;
   }
 
   const userPayload = {
     identifier: username.value,
-    firstname: firstname.value,
-    lastname: lastname.value,
-    email: email.value,
-    password: password.value,
-    role: role.value,
-    universityDepartment : universityDepartment.value
+    role: role.value
   };
 
   try {
     await AuthService.register(userPayload);
     await router.push('/users-management');
   } catch (error) {
-    console.error("Erreur Inscription:", error);
     if (error.response && error.response.data) {
       errorMessage.value = String(error.response.data);
     } else {
@@ -69,65 +51,16 @@ const fetchRoles = async () => {
   }
 };
 
-const fetchDepartment = async () => {
-  const response = await api.get('/universityDepartment/getUniversityDepartments');
-  universityDepartments.value = response.data;
-}
-
 onMounted(async () => {
-  await fetchDepartment();
   await fetchRoles();
-})
+});
 </script>
 
 <template>
-  <Sidebar/>
-  <AppHeader title="Inscription"/>
+  <Sidebar />
+  <AppHeader title="Inscription" />
   <main class="main-content">
     <form class="login-card" @submit.prevent="handleRegister">
-
-      <div class="form-group">
-        <label for="lastname">Nom</label>
-        <input
-            type="text"
-            id="lastname"
-            placeholder="Nom"
-            v-model="lastname"
-            required
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="firstname">Prénom</label>
-        <input
-            type="text"
-            id="firstname"
-            placeholder="Prénom"
-            v-model="firstname"
-            required
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="universityDepartment">Département de l'utilisateur</label>
-        <select id="universityDepartment" v-model="universityDepartment" required>
-          <option value="" disabled selected>-- Veuillez choisir un département --</option>
-          <option v-for="dept in universityDepartments" :key="dept" :value="dept">
-            {{ dept.universityDepartmentName }}
-          </option>
-        </select>
-      </div>
-
-      <div class="form-group">
-        <label for="email">Adresse mail</label>
-        <input
-            type="email"
-            id="email"
-            placeholder="exemple@email.com"
-            v-model="email"
-            required
-        />
-      </div>
 
       <div class="form-group">
         <label for="username">Identifiant (Login)</label>
@@ -136,28 +69,6 @@ onMounted(async () => {
             id="username"
             placeholder="Identifiant"
             v-model="username"
-            required
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="password">Mot de passe</label>
-        <input
-            type="password"
-            id="password"
-            placeholder="Mot de passe"
-            v-model="password"
-            required
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="confirm_password">Confirmer le mot de passe</label>
-        <input
-            type="password"
-            id="confirm_password"
-            placeholder="Confirmer le mot de passe"
-            v-model="confirmPassword"
             required
         />
       </div>
@@ -195,6 +106,7 @@ onMounted(async () => {
   font-weight: bold;
   text-align: center;
 }
+
 .main-content {
   margin-top: 200px;
   background-color: #FFFFFF;
@@ -204,6 +116,7 @@ onMounted(async () => {
   font-family: 'Roboto', sans-serif;
   padding: 0 20px 20px;
 }
+
 .login-card {
   background-color: #FFFFFF;
   padding: 2.5rem;
@@ -213,9 +126,11 @@ onMounted(async () => {
   width: 100%;
   max-width: 400px;
 }
+
 .form-group {
   margin-bottom: 1.5rem;
 }
+
 .form-group label {
   display: block;
   margin-bottom: 0.5rem;
@@ -223,7 +138,8 @@ onMounted(async () => {
   font-size: 0.9rem;
   color: black;
 }
-.form-group input {
+
+.form-group input, .form-group select {
   width: 100%;
   padding: 0.75rem;
   border: 1px solid #dcdcdc;
@@ -231,21 +147,14 @@ onMounted(async () => {
   box-sizing: border-box;
   font-size: 1rem;
 }
-.form-group input:focus, .form-group select:focus {
+
+.form-group input:focus,
+.form-group select:focus {
   outline: none;
   border-color: #B51621;
   box-shadow: 0 0 0 2px rgba(181, 22, 33, 0.2);
 }
-.form-group select {
-  appearance: menulist-button;
-  width: 100%;
-  padding: 12px 16px;
-  background: rgba(255,255,255,0.4);
-  font-size: 16px;
-  border: 1px solid #dcdcdc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+
 .login-button {
   width: 100%;
   padding: 0.8rem;
@@ -258,9 +167,11 @@ onMounted(async () => {
   cursor: pointer;
   transition: background-color 0.2s ease;
 }
+
 .login-button:hover {
   background-color: #9c121b;
 }
+
 .quit-btn {
   width: 150px;
   padding: 0.8rem;
@@ -272,11 +183,9 @@ onMounted(async () => {
   font-size: 1rem;
   font-weight: bold;
   cursor: pointer;
-  transition: background-color 0.2s ease;
-  position: relative;
   margin: 5% auto;
-  font-family: 'Roboto', sans-serif;
 }
+
 .quit-btn:hover {
   background: #999999;
   transform: translateY(-4px);
