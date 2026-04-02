e5eb7304b9cdd199fe848597975c7b9cf8ae65f6
hbequet <206597685+hbequet@users.noreply.github.com>
2026-04-02 18:47:20 +0200

    fix d'un bug empêchant la suppression de roles

--- erp_back/src/main/java/fr/iut_unilim/erp_back/service/RoleService.java
+++ erp_back/src/main/java/fr/iut_unilim/erp_back/service/RoleService.java
@@ -1,5 +1,6 @@
 package fr.iut_unilim.erp_back.service;
 
+import fr.iut_unilim.erp_back.ErpBackApplication;
 import fr.iut_unilim.erp_back.dto.RoleResponse;
 import fr.iut_unilim.erp_back.entity.Connection;
 import fr.iut_unilim.erp_back.entity.Role;
@@ -41,16 +42,21 @@ public class RoleService {
     }
 
     public boolean deleteRole(Long roleId) {
+        if (roleId == 1) return false;
+
         Optional<Role> roleOptional = roleRepository.findById(roleId);
+        ErpBackApplication.LOGGER.info("roleOptional: " + roleOptional);
         if (roleOptional.isEmpty()) return false;
 
         List<Connection> connections = connectionRepository.findAllByRole(roleOptional.get());
 
+        ErpBackApplication.LOGGER.info("connections: " + connections);
         if (!connections.isEmpty()) return false;
 
         Optional<RolePermission> permissionOptional = permissionRepository.findById(roleId);
-        if (permissionOptional.isEmpty()) return false;
-        permissionRepository.deleteById(roleId);
+        if (permissionOptional.isPresent()) {
+            permissionRepository.deleteById(roleId);
+        }
 
         roleRepository.deleteById(roleId);
 
