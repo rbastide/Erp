290da6f513f366df9d8f0578662cb22d3c138914
Reia TAERO <reiahoani.taero@etu.unilim.fr>
2026-04-02 11:56:26 +0200

    Modification légère dans le front.

--- erp_front/src/components/Admin/RecallManagement.vue
+++ erp_front/src/components/Admin/RecallManagement.vue
@@ -22,10 +22,10 @@ const editedReminder = reactive({
 });
 
 const monthsList = [
-  {value: 1, name: 'Janvier'}, {value: 2, name: 'Février'}, {value: 3, name: 'Mars'},
-  {value: 4, name: 'Avril'}, {value: 5, name: 'Mai'}, {value: 6, name: 'Juin'},
-  {value: 7, name: 'Juillet'}, {value: 8, name: 'Août'}, {value: 9, name: 'Septembre'},
-  {value: 10, name: 'Octobre'}, {value: 11, name: 'Novembre'}, {value: 12, name: 'Décembre'}
+  {value: 1, name: 'Septembre'}, {value: 2, name: 'Octobre'}, {value: 3, name: 'Novembre'},
+  {value: 4, name: 'Décembre'}, {value: 5, name: 'Janvier'}, {value: 6, name: 'Février'},
+  {value: 7, name: 'Mars'}, {value: 8, name: 'Avril'}, {value: 9, name: 'Mai'},
+  {value: 10, name: 'Juin'}, {value: 11, name: 'Juillet'}, {value: 12, name: 'Août'}
 ];
 
 const getMonthName = (val: number) => monthsList.find(m => m.value === val)?.name || '';
